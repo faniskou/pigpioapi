30fa524f0bf98bc47e8561fbfdcbc22521bb264d
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-11 21:53:48 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(4, GPIO.OUT) 
+GPIO.setup(13, GPIO.OUT) 
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
 
 
@@ -19,12 +19,12 @@ def button_callback(gp):
 
 @app.route('/on')
 def turn_led_on():
-    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(13, GPIO.HIGH)
     return "OK"
 
 @app.route('/off')
 def turn_led_off():
-    GPIO.output(4, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
     return "OK"
 
 if __name__ == '__main__':
