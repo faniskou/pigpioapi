38e3c196cd38d06118d0467dae1e1b886ff22ed9
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-11 21:39:40 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -1,20 +1,31 @@
-
-try:
-    from flask import Flask
-    from flask_restful import Resource,Api
-    from flask_restful import reqparse
-    from flask import request
-    from gpiozero import LED, Button
-    from signal import pause
-    import time
-    import datetime
-    import json
-    import threading
+import RPi.GPIO as GPIO
+from flask import Flask
+app = Flask(__name__)
 
 
-led = LED(17)
-button = Button(2)
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(4, GPIO.OUT 
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-# button.when_pressed = led.on
-# button.when_released = led.off
 
+
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+
+
+
+def button_callback(channel):
+    print("Button was pushed!")
+
+@app.route('/on')
+def turn_led_on():
+    GPIO.output(4, GPIO.HIGH)
+    return "OK"
+
+@app.route('/off')
+def turn_led_off():
+    GPIO.output(4, GPIO.LOW)
+    return "OK"
+
+if __name__ == '__main__':
+    app.run()
