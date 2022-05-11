f40e527f2379ccc03e427f0eb676b0e329944267
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-11 21:40:14 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(4, GPIO.OUT 
+GPIO.setup(4, GPIO.OUT) 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 
