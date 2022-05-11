4bc09b6aa9d3ec46607230a5ae39f991ea24fa92
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-11 22:01:37 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -8,10 +8,10 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(13, GPIO.OUT) 
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
 
-def button_callback(gp):
-    print("Button was pushed!" + gp)
+def button_callback(channel):
+    print("Button was pushed!" + channel)
 
-GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback("3")) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
 
 
