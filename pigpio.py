7026e26a0d28273f5a47911fe9dc0d2c319ea42e
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-12 22:07:30 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -17,7 +17,12 @@ list_out_http =  ["http://192.168.0.100:8091/hitthebell"]
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT) 
 GPIO.setup(13, GPIO.OUT) 
+GPIO.setup(15, GPIO.OUT) 
+
+
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
 URL = "http://192.168.0.100:8091/hitthebell"
 def button_callback(channel):
