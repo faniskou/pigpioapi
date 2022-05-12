e81b387c814b5108b6c1bdb344596392d36b92c6
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-12 22:02:56 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -32,12 +32,12 @@ GPIO.add_event_detect(3,GPIO.FALLING,callback=button_callback, bouncetime=200) #
 
 
 @app.route('/<gpionum>/on')
-def turn_led_on():
+def turn_led_on(gpionum):
     GPIO.output(gpionum, GPIO.HIGH)
     return "OK"
 
 @app.route('/<gpionum>/off')
-def turn_led_off():
+def turn_led_off(gpionum):
     GPIO.output(gpionum, GPIO.LOW)
     return "OK"
 
