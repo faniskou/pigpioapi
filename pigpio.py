6828735d6c3c813d1e2c538cd0ee962033c8dc94
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-14 17:55:22 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -32,7 +32,7 @@ def button_callback(channel):
     except requests.exceptions.RequestException as e:  print(e)
    
 
-GPIO.add_event_detect(3,GPIO.FALLING,callback=button_callback, pull_up_down=GPIO.PUD_UP) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(3,GPIO.rizing,callback=button_callback) # Setup event on pin 10 rising edge
 
 
 
