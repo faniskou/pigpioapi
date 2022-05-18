a0d44f57cca546b87aa475c26c8a567a541ac4ef
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-18 11:04:18 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 
 # TODO
 
-list_in = {4:LED(4),17:LED(17),27:LED(27),22:LED(22)}
-list_out = {2:"http://192.168.0.100:8091/hitthebell"}
-list_out_http =  []
+list_in = [4,17,27,22]
+list_out = [2]
+list_out_http =  ["http://192.168.0.100:8091/hitthebell"]
 # add in status 
 # check in is in the list and loop outs 
 #[1,2,3].index(2) # => 1
 #[1,2,3].index(4) # => ValueError
 
+sw1 = LED(4)
+sw2 = LED(17)
+sw2 = LED(27)
+sw2 = LED(22)
 
 bt1 = Button(2)
 
@@ -22,21 +26,21 @@ URL = "http://192.168.0.100:8091/hitthebell"
 def button_callback_bt1():
     print("Button was pushed!")
     try:
-       requests.get(list_out[2])
+       requests.get(URL)
     except requests.exceptions.RequestException as e:  print(e)
    
 
 bt1.when_pressed = button_callback_bt1
 
 
-@app.route('/<gpionum>/on')
+@app.route('/sw1/on')
 def turn_led_on(gpionum):
-    list_in[int(gpionum)].on()
+    sw1.on()
     return "OK"
 
-@app.route('/<gpionum>/off')
+@app.route('/sw1/off')
 def turn_led_off(gpionum):
-    list_in[int(gpionum)].off()
+    sw1.off()
     return "OK"
 
 
