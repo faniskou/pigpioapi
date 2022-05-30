13e663239140ebf0f79e959f8d4f899394424ce6
Kouvaris Theofanis <faniskou@gmail.com>
2022-05-30 18:16:19 +0300

    Update pigpio.py

--- pigpio.py
+++ pigpio.py
@@ -45,7 +45,7 @@ def turn_led_off(gpionum):
 
 
 @app.route('/ha/sw1')
-def turn_led_off(gpionum):
+def haled(gpionum):
     if(sw1().is_lit):
       return  '{"active": "true"}'
     return  '{"active": "false"}'
